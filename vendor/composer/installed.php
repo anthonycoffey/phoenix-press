1c71f69eb4519f44dccef1d88c6e12919f240f8b
Anthony Coffey <coffey.j.anthony@gmail.com>
2024-10-05 04:08:10 -0500

    adding back `vendor` folder for better developer experience when switching machines

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'phoenix/press',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8b2a16c7961ccbec943179ddb8184bb812072889',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phoenix/press' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8b2a16c7961ccbec943179ddb8184bb812072889',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
