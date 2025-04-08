838d924ca431c430e27e02c7d0c2d1709c91c32c
Anthony Coffey <coffey.j.anthony@gmail.com>
2025-04-07 22:50:58 -0500

    ...

---
+++ build/auto/625.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '325764211271274737c4');
---
+++ build/auto/main.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => 'e33423cd6a91b92c9ad1');
