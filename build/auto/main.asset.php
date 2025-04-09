d8da4647c03cf74a1e9030500b2bb3897aecc01b
Anthony Coffey <coffey.j.anthony@gmail.com>
2025-04-09 04:54:28 -0500

    styling updates for production (chat widget)

--- build/auto/main.asset.php
+++ build/auto/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => 'b758834a7857113a7b98');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => '2394cf4fa4f11440ac35');
